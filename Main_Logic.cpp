7e8480875b6a3125524ed1e8f0e2619b43914940
Fred Yagofarov <fred-yagofarov13@psu.ru>
2024-12-03 10:25:50 +0500

    исправлено название функции main

--- Main_Logic.cpp
+++ Main_Logic.cpp
@@ -6,66 +6,6 @@
 #include "Pyatnashky_Logic.h";
 using namespace std;
 
-
-
-int start() {
-    setlocale(LC_ALL, "Russian");
-
-    int restart;
-    do {
-        int matrix[4][4];
-        fillRandomNumbers(matrix);
-
-        int moveCount = 0;
-
-        while (true) {
-            clearScreen();
-            printMatrix(matrix);
-
-            if (isSorted(matrix)) {
-                cout << "Поздравляем! Вы собрали пятнашки!" << endl;
-                cout << "Количество ходов: " << moveCount << endl;
-                break;
-            }
-
-            cout << "Используйте стрелки для перемещения плиток. Esc - выход." << endl;
-
-            int key = _getch();
-
-            if (key == 224) {
-                key = _getch();
-                switch (key) {
-                    case 72:
-                        moveUp(matrix, moveCount);
-                    break;
-                    case 80:
-                        moveDown(matrix, moveCount);
-                    break;
-                    case 75:
-                        moveLeft(matrix, moveCount);
-                    break;
-                    case 77:
-                        moveRight(matrix, moveCount);
-                    break;
-                    default:
-                        break;
-                }
-            }
-            else if (key == 27) {
-                break;
-            }
-        }
-
-        cout << "Хотите сыграть ещё раз? (1 - да, 0 - нет): ";
-        cin >> restart;
-
-    } while (restart != 0);
-
-    return 0;
-}
-
-
-
 void moveLeft(int matrix[4][4], int& moveCount) {
     int iZero, jZero;
     findZero(matrix, iZero, jZero);
@@ -126,4 +66,61 @@ bool isSorted(int matrix[4][4]) {
         }
     }
     return true;
+}
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int restart;
+    do {
+        int matrix[4][4];
+        fillRandomNumbers(matrix);
+
+        int moveCount = 0;
+
+        while (true) {
+            clearScreen();
+            printMatrix(matrix);
+
+            if (isSorted(matrix)) {
+                cout << "Поздравляем! Вы собрали пятнашки!" << endl;
+                cout << "Количество ходов: " << moveCount << endl;
+                break;
+            }
+
+            cout << "Используйте стрелки для перемещения плиток. Esc - выход." << endl;
+
+            int key = _getch();
+
+            if (key == 224) {
+                key = _getch();
+                switch (key) {
+                    case 72:
+                        moveUp(matrix, moveCount);
+                    break;
+                    case 80:
+                        moveDown(matrix, moveCount);
+                    break;
+                    case 75:
+                        moveLeft(matrix, moveCount);
+                    break;
+                    case 77:
+                        moveRight(matrix, moveCount);
+                    break;
+                    default:
+                        break;
+                }
+            }
+            else if (key == 27) {
+                break;
+            }
+        }
+
+        cout << "Хотите сыграть ещё раз? (1 - да, 0 - нет): ";
+        cin >> restart;
+
+    } while (restart != 0);
+
+    return 0;
 }
